906928b566208512dd9668908a917fa936ab32af
Jessie Waite and Joseph Tomlinson <jessie@>
2017-09-19 11:49:18 -0700

    Add files, HTML and JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,37 @@
 $(document).ready(function() {
-  var workTravels = ["bike", "car", "carpool", "walk", "bus", "train", "taxi", "rideshare"]
-  workTravels.forEach(function(workTravel) {
-    $("#worktravel").append(`
-      <input type="checkbox" name="work-transportation" value="`+workTravel+`"> `+workTravel+`<br>
+  var personalities = ["1. Neurotic", "2. Unorganized", "3. Energetic", "4. Lethargic", "5. Anxious", "6. Pessimistic", "7. Optomistic", "8. Introverted", "9. Extroverted"]
+  personalities.forEach(function(personality) {
+    $("#inputtypes").append(`
+      <input type="checkbox" name="personality-type" value="`+personality+`"> `+personality+`<br>
     `);
   });
-  var funTravels = ["bike", "car", "scooter", "motorcycle", "skateboard", "rideshare", "fly"]
-  funTravels.forEach(function(funTravel) {
-    $("#funtravel").append(`
-      <input type="checkbox" name="fun-transportation" value="` +funTravel+`"> `+funTravel+` <br>
-    `);
-  });
-  $("form#travel").submit(function(event) {
+
+  $("#personality").submit(function(event) {
     event.preventDefault();
-    $("#output1").removeClass("hidden");
-    $("input:checkbox[name=work-transportation]:checked").each(function() {
-      var workTravelMode = $(this).val();
-      $("#output1").append(workTravelMode + "<br>");
+    $("#output").removeClass("hidden");
+    var numbers = 0;
+    $("input:checkbox[name=personality-type]:checked").each(function() {
+      var personalityType = $(this).val();
+      var resultType = parseInt(personalityType);
+      numbers = numbers + resultType
+      console.log(numbers);
+      $("#output").append(personalityType + "<br>");
     });
-    $("#output2").removeClass("hidden");
-    $("input:checkbox[name=fun-transportation]:checked").each(function() {
-      var funTravelMode = $(this).val();
-      $("#output2").append(funTravelMode + "<br>");
-    });
+
+    if (numbers <=6) {
+      $("#output").text("You are a Doc");
+    } else if (numbers > 6 && numbers <=12){
+      $("#output").text("You are a Grumpy");
+    } else if (numbers > 12 && numbers <= 18) {
+      $("#output").text("You are a Happy");
+    } else if (numbers > 18 && numbers <= 24) {
+      $("#output").text("You are a Sleepy");
+    } else if (numbers > 24 && numbers <= 30) {
+      $("#output").text("You are a Bashful");
+    } else if (numbers > 30 && numbers <= 36) {
+      $("#output").text("You are a Sneezy");
+    } else {
+      $("#output").text("You are a Dopey");
+    }
   });
 });
